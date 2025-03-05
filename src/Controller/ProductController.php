f795024aa50a23f4c27615fbd24039815c5030ce
Wasim Hyder <wasimhyder09@gmail.com>
2025-03-05 16:26:35 +0500

    Displayed individual product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
 
-final class ProductController extends AbstractController {
-  #[Route('/product', name: 'product_index')]
+class ProductController extends AbstractController {
+  #[Route('/products', name: 'product_index')]
   public function index(ProductRepository $repository): Response {
     $products = $repository->findAll();
     return $this->render('product/index.html.twig', [
       'products' => $products,
+    ]);
+  }
+
+  #[Route('/products/{id<\d+>}', name: 'product_show')]
+  public function show(Product $product): Response {
+    return $this->render('product/show.html.twig', [
+      'product' => $product
     ]);
   }
 }
